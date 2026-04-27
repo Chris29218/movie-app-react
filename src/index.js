57620ae5108e0466b0664642c5cb5aa5b8a8837d
Abhishek Bhattacharya <Ab29281@gmail.com>
2026-04-27 17:43:06 +0530

    Fixing comment

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './Components/App';
 import { createStore } from 'redux';
 import movies from './reducers'
 
-// Creating Redux store (Redux 4.2 LTS compatible)
+// Creating Redux store
 const store = createStore(movies);
 
 // React 18 LTS: Using createRoot for concurrent rendering
